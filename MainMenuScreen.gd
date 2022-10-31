4be6ac5c35915b296464d846c1a93e3c84294b5c
RebelliousKiwi04 <cjsandlant@gmail.com>
2022-10-31 22:39:18 +1300

    Create Main Menu

---
+++ MainMenuScreen.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+var maps = [
+	"res://Main.tscn"
+	]
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+func _on_StartButton_pressed():
+	randomize()
+	get_tree().change_scene(maps[rand_range(0,maps.size()-1)])
+
+
+func _on_HighScoreButton_pressed():
+	pass # Replace with function body.
